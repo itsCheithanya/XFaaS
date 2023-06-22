64fcea5a00de18beef8acf82a2a8892a3646d5c8
Haarish Anandan <131394736+haarish04@users.noreply.github.com>
2023-06-22 13:11:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Wf from './components/Wf';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CodeViewer from './components/CodeViewer';
+import InvocationsPage from './InvocationsPage';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<App/>} />
+        <Route path="/wf" element={<Wf/>}/>
+        <Route path="/wf/CodeViewer" element={<CodeViewer/>} />
+        <Route path="/wf/InvocationsPage" element={<InvocationsPage/>} />
+
+        
+      </Routes>
+    </Router>
+
   </React.StrictMode>
 );
 
