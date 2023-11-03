38f1b1cdf39c5eaf4ae790016a6a601058e42469
Haarish Anandan <131394736+haarish04@users.noreply.github.com>
2023-11-03 12:08:55 +0530

    Update InvocationDetails.js

--- src/scenes/invocations/InvocationDetails.js
+++ src/scenes/invocations/InvocationDetails.js
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from '@mui/material';
+import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Button, MenuItem, Select } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import InvocationData from './Data';
 
 const InvocationDetails = () => {
   const [filterValue, setFilterValue] = useState('');
+  const [filterTime, setFilterTime] = useState('all'); 
   const [invocationList, setInvocationList] = useState([]);
   const { id } = useParams();
 
   const convertInvocationList = () => {
     const instance = InvocationData.find(data => data.workflow_invocation_id === id);
-    const invocationStartTime = instance ? instance.invocation_start_time_ms : 0; // Fetch the invocation start time
+    const invocationStartTime = instance ? instance.invocation_start_time_ms : 0; 
     const functions = instance ? instance.functions : {};
 
     const convertMsToDateTime = (ms) => {
-      const date = new Date(invocationStartTime + ms); // Calculate date based on the invocation start time
+      const date = new Date(invocationStartTime + ms); 
       return date.toLocaleString();
     };
 
@@ -33,45 +34,71 @@ const InvocationDetails = () => {
     setInvocationList(formattedInvocationList);
   }, [id]);
 
-  //useEffect(() => {
-  //  const fetchData = async () => {
-  //    try {
-  //      const response = await fetch('YOUR_API_ENDPOINT/' + id); // Replace 'YOUR_API_ENDPOINT' with your actual API endpoint
-  //      if (!response.ok) {
-  //        throw new Error('Network response was not ok');
-  //      }
-  //      const data = await response.json();
-  //      const formattedInvocationList = convertInvocationList(data);
-  //      setInvocationList(formattedInvocationList);
-  //    } catch (error) {
-  //      console.error('Error fetching data:', error);
-        // Handle errors or set a default state here if the fetch fails
-  //    }
-  //  };
-
-  //  fetchData();
-  //}, [id]);
-
   const handleFilterChange = (event) => {
     setFilterValue(event.target.value);
   };
 
-  // Filter and sort functions based on start time in ascending order
+  const handleFilterByTime = (timeValue) => {
+    setFilterTime(timeValue);
+  };
+
+  const applyTimeFilter = (filteredData) => {
+    if (filterTime === 'all') {
+      return filteredData;
+    } else {
+      const now = Date.now();
+      const filterTimes = {
+        last1Hour: now - 3600000,
+        lastDay: now - 86400000,
+        lastMonth: now - 2629746000,
+        last6Months: now - 15778476000,
+        last12Months: now - 31556952000,
+      };
+  
+      return filteredData.filter(([_key, value]) => {
+        const startTime = new Date(value.start_time_formatted).getTime();
+        return startTime > filterTimes[filterTime];
+      });
+    }
+  };
+
   const filteredInvocationList = Object.entries(invocationList)
     .filter(([key]) => key.startsWith(filterValue))
     .sort(([, a], [, b]) => b.end_delta_ms - a.end_delta_ms);
 
+  const filteredByTime = applyTimeFilter(filteredInvocationList);
+
   return (
     <div>
-      <Box display="flex" flexDirection="column" alignItems="center">
-        <Box p={2} paddingTop='10%'>
-          <TextField
-            label="Filter by ID"
-            value={filterValue}
-            onChange={handleFilterChange}
-            variant="outlined"
-          />
+      <Box p={2} display="flex" alignItems="center" paddingTop='10%'>
+          <Box mr={2}>
+            <TextField
+              label="Filter by ID"
+              value={filterValue}
+              onChange={handleFilterChange}
+              variant="outlined"
+            />
+          </Box>
+          <Box>
+            <Select
+              value={filterTime}
+              onChange={(event) => handleFilterByTime(event.target.value)}
+              variant="outlined"
+              displayEmpty
+            >
+              <MenuItem value="" disabled>
+                Filter by Time
+              </MenuItem>
+              <MenuItem value="all">All</MenuItem>
+              <MenuItem value="last1Hour">Last 1 hour</MenuItem>
+              <MenuItem value="lastDay">Last day</MenuItem>
+              <MenuItem value="lastMonth">Last month</MenuItem>
+              <MenuItem value="last6Months">Last 6 months</MenuItem>
+              <MenuItem value="last12Months">Last 12 months</MenuItem>
+            </Select>
+          </Box>
         </Box>
+
         <Box width="70%" p={5} border="2px solid blue" margin="0 auto" marginTop="2%" marginBottom='10%'>
           <TableContainer sx={{ bgcolor: 'background.paper' }}>
             <Table>
@@ -84,7 +111,7 @@ const InvocationDetails = () => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {filteredInvocationList.map(([key, value]) => {
+                {filteredByTime.map(([key, value]) => {
                   const executionTime = value.end_delta_ms - value.start_delta_ms;
                   return (
                     <TableRow key={key}>
@@ -98,7 +125,7 @@ const InvocationDetails = () => {
               </TableBody>
             </Table>
           </TableContainer>
-        </Box>
+        
       </Box>
     </div>
   );
