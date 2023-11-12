cc8677848c61888bb2191743c1235e9cf61684fe
Haarish Anandan <131394736+haarish04@users.noreply.github.com>
2023-11-12 20:17:51 +0530

    Update StartvsEnd.jsx

--- src/components/StartvsEnd.jsx
+++ src/components/StartvsEnd.jsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ResponsiveScatterPlot } from '@nivo/scatterplot';
 import output from '../scenes/line/StartvsEndData';
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 
 const StartvsEndScatterPlot = () => {
-  const XsortedData = output
+  const [selectedFilter, setSelectedFilter] = useState('all');
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    const filterData = () => {
+      const currentDate = new Date();
+      switch (selectedFilter) {
+        case 'lastDay':
+          setFilteredData(
+            output.filter(item => {
+              const startTime = new Date(item.invocation_start_time_ms);
+              return currentDate - startTime <= 24 * 60 * 60 * 1000; // 24 hours
+            })
+          );
+          break;
+        case 'lastMonth':
+          setFilteredData(
+            output.filter(item => {
+              const startTime = new Date(item.invocation_start_time_ms);
+              return currentDate - startTime <= 30 * 24 * 60 * 60 * 1000; // 30 days
+            })
+          );
+          break;
+        case 'last6Months':
+          setFilteredData(
+            output.filter(item => {
+              const startTime = new Date(item.invocation_start_time_ms);
+              return currentDate - startTime <= 6 * 30 * 24 * 60 * 60 * 1000; // 6 months
+            })
+          );
+          break;
+        case 'last12Months':
+          setFilteredData(
+            output.filter(item => {
+              const startTime = new Date(item.invocation_start_time_ms);
+              return currentDate - startTime <= 12 * 30 * 24 * 60 * 60 * 1000; // 12 months
+            })
+          );
+          break;
+        case 'all':
+        default:
+          setFilteredData(output);
+          break;
+      }
+    };
+
+    filterData(); 
+  }, [selectedFilter]);
+
+  const XsortedData = filteredData
     .map(item => ({
       x: new Date(item.invocation_start_time_ms).toLocaleString(),
       y: new Date(item.workflowEndTime).toLocaleString(),
@@ -12,59 +62,79 @@ const StartvsEndScatterPlot = () => {
 
   const YsortedData = [...XsortedData].sort((a, b) => new Date(a.y) - new Date(b.y));
 
-  // Create formatted data
   const formattedData = [{ id: 'invocations', data: XsortedData }];
 
-  const startX = XsortedData[0].x;
-  const endX = XsortedData[XsortedData.length - 1].x;
-  const startY = YsortedData[0].y;
-  const endY = YsortedData[YsortedData.length - 1].y;
+  const startX = XsortedData[0]?.x || '';
+  const endX = XsortedData[XsortedData.length - 1]?.x || '';
+  const startY = YsortedData[0]?.y || '';
+  const endY = YsortedData[YsortedData.length - 1]?.y || '';
 
   return (
-    <ResponsiveScatterPlot
-      data={formattedData}
-      margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-      xScale={{ type: 'point' }}
-      yScale={{ type: 'point' }}
-      colors="lightblue"
-      blendMode="normal"
-      enableGridX={true}
-      enableGridY={true}
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{
-        tickSize: 0,
-        tickPadding: 5,
-        legend: 'Start Time',
-        legendPosition: 'middle',
-        legendOffset: 32,
-        tickValues: [startX, endX],
-      }}
-      axisLeft={{
-        tickSize: 0,
-        tickPadding: 5,
-        legend: 'End Time',
-        legendPosition: 'middle',
-        legendOffset: -40,
-        tickValues: [startY, endY],
-      }}
-      nodeSize={8}
-      tooltip={({ node }) => (
-        <div
-          style={{
-            background: 'white',
-            padding: '9px 12px',
-            border: '1px solid #ccc',
-            borderRadius: '5px',
-          }}
+    <>
+      <FormControl>
+        <InputLabel id="filter-label">Filter</InputLabel>
+        <Select
+          labelId="filter-label"
+          id="filter-select"
+          value={selectedFilter}
+          onChange={(e) => setSelectedFilter(e.target.value)}
         >
-          <strong>Start Time: {node.data.x}</strong>
-          <br />
-          <strong>End Time: {node.data.y}</strong>
-        </div>
-      )}
+          <MenuItem value="all">All</MenuItem>
+          <MenuItem value="lastDay">Last Day</MenuItem>
+          <MenuItem value="lastMonth">Last Month</MenuItem>
+          <MenuItem value="last6Months">Last 6 Months</MenuItem>
+          <MenuItem value="last12Months">Last 12 Months</MenuItem>
+        </Select>
+      </FormControl>
       
-    />
+      <ResponsiveScatterPlot
+        data={formattedData}
+        margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+        xScale={{ 
+          type: 'point',
+          min: startX,
+          max: endX,
+         }}
+        yScale={{ type: 'point', min: startY, max: endY }}
+        colors="lightblue"
+        blendMode="normal"
+        enableGridX={true}
+        enableGridY={true}
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          tickSize: 0,
+          tickPadding: 5,
+          legend: 'Start Time',
+          legendPosition: 'middle',
+          legendOffset: 32,
+          tickValues: [startX, endX],
+        }}
+        axisLeft={{
+          tickSize: 0,
+          tickPadding: 5,
+          legend: 'End Time',
+          legendPosition: 'middle',
+          legendOffset: -40,
+          tickValues: [startY, endY],
+        }}
+        nodeSize={8}
+        tooltip={({ node }) => (
+          <div
+            style={{
+              background: 'white',
+              padding: '9px 12px',
+              border: '1px solid #ccc',
+              borderRadius: '5px',
+            }}
+          >
+            <strong>Start Time: {node.data.x}</strong>
+            <br />
+            <strong>End Time: {node.data.y}</strong>
+          </div>
+        )}
+      />
+    </>
   );
 };
 
